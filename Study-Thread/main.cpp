c32a3dd8d933b95a306b4f0d8f47f3f311fb11fd
JangHyunsoo <hunsoo911@gmail.com>
2021-07-18 15:17:13 +0900

    tag : first commit

---
+++ Study-Thread/main.cpp
@@ -0,0 +1,12 @@
+﻿#include <iostream>
+#include <thread>
+
+using namespace std;
+
+int main() {
+	thread t1 = thread([](const string& strMsg){cout << strMsg << endl; }, "hello");
+
+	t1.join();
+
+	return 0;
+}
